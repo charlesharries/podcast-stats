6e5ea3f54816d90f329ca9e69e60b5de00511dcc
Charles Harries <charles@harries.me>
2020-07-20 23:35:14 +0100

    Revert "Revert FileServer config"
    This reverts commit 716ccdb1a4c24d0c551882157220ef206999decf.

--- cmd/app/routes.go
+++ cmd/app/routes.go
@@ -44,8 +44,8 @@ func (app *application) routes() http.Handler {
 
 	mux.Get("/ping", http.HandlerFunc(ping))
 
-	fileServer := http.FileServer(http.Dir("./static"))
-	mux.Get("/static/", http.StripPrefix("/static", fileServer))
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Get("/static/", http.StripPrefix("/static/", fileServer))
 
 	return standardMiddleware.Then(mux)
 }
