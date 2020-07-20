54bff1b4c2a4eec33b78cfb786d20fb3aaf52783
Charles Harries <charles@harries.me>
2020-07-20 23:27:58 +0100

    Another deploy config attempt

--- cmd/app/routes.go
+++ cmd/app/routes.go
@@ -44,7 +44,7 @@ func (app *application) routes() http.Handler {
 
 	mux.Get("/ping", http.HandlerFunc(ping))
 
-	fileServer := http.FileServer(http.Dir("./static"))
+	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Get("/static/", http.StripPrefix("/static/", fileServer))
 
 	return standardMiddleware.Then(mux)
